926a76447892388af09e5276ca2528b8b1dc8814
Mile Divovic <md2950@nyu.edu>
2020-03-31 14:32:27 -0700

    PS-139 add adapters.ts, initial interface for dispatch plugins

---
+++ adapters.ts
@@ -0,0 +1,12 @@
+export interface Dispatch {
+	//TODO: define this ServiceMessage
+	getQueuedMessage(): ServiceMessage; //Get the next highest priority message from the queue
+	
+	dispatchToLrs(message: ServiceMessage): void; //Dispatch the message to the LRS component plugin
+	dispatchToDatabase(message: ServiceMessage): void; //Dispatch the message to the DB component plugin
+	dispatchToClient(message: ServiceMessage): void; //Dispatch the message to the connection manager to go back to the client
+	dispatchToCache(message: ServiceMessage): void; //Dispatch the message to the user session data plugin
+
+	//TODO: Should this return a const value representing the target for dispatch?
+	determineDispatchTarget(message: ServiceMessage): void; //Based on the message, determine which target to dispatch to
+}
