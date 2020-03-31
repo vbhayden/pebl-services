67767ed6d6295e69915c67cc9b60f71bb4283746
Mile Divovic <md2950@nyu.edu>
2020-03-31 15:02:37 -0700

    PS-143 outline for Message Queue plugins

--- adapters.ts
+++ adapters.ts
@@ -1,7 +1,9 @@
+import {ServiceMessage} from './models';
+
 export interface Dispatch {
 	//TODO: define this ServiceMessage
 	getQueuedMessage(): ServiceMessage; //Get the next highest priority message from the queue
-	
+
 	dispatchToLrs(message: ServiceMessage): void; //Dispatch the message to the LRS component plugin
 	dispatchToDatabase(message: ServiceMessage): void; //Dispatch the message to the DB component plugin
 	dispatchToClient(message: ServiceMessage): void; //Dispatch the message to the connection manager to go back to the client
@@ -9,4 +11,10 @@ export interface Dispatch {
 
 	//TODO: Should this return a const value representing the target for dispatch?
 	determineDispatchTarget(message: ServiceMessage): void; //Based on the message, determine which target to dispatch to
+}
+
+export interface MessageQueue {
+	enqueueMessage(message: ServiceMessage): void; //Place the message into the queue
+
+	//TODO: Is there a priority for messages?
 }
