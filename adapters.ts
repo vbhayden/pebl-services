f9bbcfd5f242fd174a068fd206405da9c3e5a2ed
Mile Divovic <md2950@nyu.edu>
2020-03-31 17:36:47 -0700

    PS-144 add plugin code outline for session data cache plugins

--- adapters.ts
+++ adapters.ts
@@ -1,4 +1,4 @@
-import {ServiceMessage} from './models';
+import {ServiceMessage, UserProfile, Annotation, SharedAnnotation, XApiStatement, Competency, Message} from './models';
 
 export interface Dispatch {
 	//TODO: define this ServiceMessage
@@ -17,4 +17,41 @@ export interface MessageQueue {
 	enqueueMessage(message: ServiceMessage): void; //Place the message into the queue
 
 	//TODO: Is there a priority for messages?
+}
+
+export interface SessionDataCache {
+	//TODO: Are xAPI statements being stored in the cache or a different format for the data?
+	getAnnotationsForBook(userProfile: UserProfile, book: string): Annotation[]; //Retrieve annotations made by the user within a specific book
+	getAnnotations(userProfile: UserProfile): Annotation[]; //Retrieve annotations made by the user across all books
+	saveAnnotationsForBook(userProfile: UserProfile, book: string, stmts: Annotation[]): void; //Store annotations made by the user within the specific book
+
+	getSharedAnnotationsForBook(userProfile: UserProfile, book: string): SharedAnnotation[]; //Retrieve shared annotations visible to the user made within a specific book
+	getSharedAnnotations(userProfile: UserProfile): SharedAnnotation[]; //Retrieve shared annotations visible to the user made across all books
+	saveSharedAnnotationsForBook(userProfile: UserProfile, book: string, stmts: SharedAnnotation[]): void; //Store shared annotations visible to the user made within the specific book
+
+	removeAnnotation(userProfile: UserProfile, id: string): void; //Removes the annotation with the specific id
+	removeSharedAnnotation(userProfile: UserProfile, id: string): void; //Removes the shared annotation with the specific id
+
+
+	getEventsForBook(userProfile: UserProfile, book: string): XApiStatement[]; //Retrieve all events for this user made within the specific book
+	getEvents(userProfile: UserProfile): XApiStatement[]; //Retrieve all events for this user
+	saveEventsForBook(userProfile: UserProfile, book: string, events: XApiStatement[]): void; // Store the events for this user made within the specific book
+	saveEvents(userProfile: UserProfile, events: XApiStatement[]): void; // Store the events for this user
+	removeEvent(userProfile: UserProfile, id: string): void; //Removes the event with the specified id
+
+
+	getCompetencies(userProfile: UserProfile): Competency[]; //Retrueve competencies for this user
+	saveCompetencies(userProfile: UserProfile, competencies: Competency[]): void; //Store competencies for this user
+	removeCompetency(userProfile: UserProfile, id: string): void; //Removes the competency with the specified id
+
+
+	getMessages(userProfile: UserProfile, thread: string): Message[]; //Retrieve messages for the specified thread
+	saveMessages(userProfile: UserProfile, thread: string, messages: Message[]): void; //Store messages for the specified thread
+	removeMessage(userProfile: UserProfile, id: string): void; //Removes the message with the specified id
+
+
+	getNotifications(userProfile: UserProfile): Notification[]; //Retrieves all notifications for this user
+	getNotificationsForBook(userProfile: UserProfile, book: string): Notification[]; //Retrieves all notifications for the specified book for this user
+	saveNotifications(userProfile: UserProfile, notifications: Notification[]): void; //Stores the notifications for this user
+	removeNotification(userProfile: UserProfile, id: string): void; //Removes the notification with the specified id
 }
