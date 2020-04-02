c9eeecc2a3277c07a3c08acddd68546f45f47223
Aaron Veden <aaron.veden@eduworks.com>
2020-04-02 16:01:02 -0700

    PS-107 PS-120 PS-121 testing basic sticky sessions backed by redis

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as bodyParser from "body-parser";
 
+import * as redis from 'redis'
 import * as fs from 'fs';
 import * as https from 'https';
 import * as http from 'http';
@@ -24,6 +25,13 @@ let cert
 let credentials: { [key: string]: any } = {}
 let httpsServer
 
+let expressSession = require('express-session');
+let RedisSessionStore = require('connect-redis')(expressSession);
+
+const redisClient = redis.createClient({
+    password: config.redisAuth
+});
+
 if (config.useSSL) {
     privKey = fs.readFileSync(config.privateKeyPath, "utf8");
     cert = fs.readFileSync(config.certificatePath, "utf8");
@@ -41,6 +49,7 @@ if (config.useSSL) {
 
 expressApp = require('express-ws')(expressApp, httpsServer).app;
 
+// Potentially needed for CORS
 // var allowCrossDomain = function(req: Request, res: Response, next: Function) {
 //     let slashIndex = req.path.indexOf("/", 1);
 //     if ((slashIndex != -1) && (slashIndex > 1)) {
@@ -71,6 +80,34 @@ expressApp = require('express-ws')(expressApp, httpsServer).app;
 
 // expressApp.use(allowCrossDomain);
 
+redisClient.on("error", function(error) {
+    console.error(error);
+});
+
+
+expressApp.use(
+    expressSession({
+        store: new RedisSessionStore({ client: redisClient }),
+        secret: config.sessionSecret,
+        cookie: {
+            secure: true,
+            httpOnly: true,
+            maxAge: config.sessionTTL,
+            sameSite: "strict"
+        },
+        proxy: config.usesProxy,
+        saveUninitialized: false,
+        resave: false
+    })
+);
+
+// Make sure session exists
+// expressApp.use(function(req: Request, res: Response, next: Function) {
+//     if (!req.session) {
+//         return next(new Error("error"));
+//     }
+// });
+
 expressApp.use(bodyParser.urlencoded({ extended: false }));
 expressApp.use(bodyParser.json());
 
@@ -82,6 +119,13 @@ expressApp.use(function(req: Request, res: Response, next: Function) {
 
 expressApp.get('/', function(req: Request, res: Response) {
     console.log('get route', req.originalUrl);
+    if (req.session) {
+        if (!req.session.test) {
+            req.session.test = 0
+        }
+        req.session.test = req.session.test + 1
+    }
+    console.log('get route', req.originalUrl, req.session?.test);
     res.end();
 });
 
