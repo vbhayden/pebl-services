136cb8dde78ca607893df01b5f2b1e658d52b3cd
Mile Divovic <md2950@nyu.edu>
2020-04-08 15:06:58 -0700

    PS-114 remove extra data from endpoint class

--- src/models/endpoint.ts
+++ src/models/endpoint.ts
@@ -1,9 +1,6 @@
 export class Endpoint {
   readonly url: string;
   readonly headers: { [key: string]: any };
-  readonly username: string;
-  readonly password: string;
-  readonly token: string;
   readonly lastSyncedThreads: { [key: string]: Date }
   readonly lastSyncedBooksMine: { [key: string]: Date }
   readonly lastSyncedBooksShared: { [key: string]: Date }
@@ -13,13 +10,6 @@ export class Endpoint {
   constructor(raw: { [key: string]: any }) {
     this.url = raw.url;
     this.headers = raw.headers;
-    this.username = raw.username;
-    this.password = raw.password;
-    this.token = raw.token;
-
-    if (!this.token) {
-      this.token = btoa(this.username + ":" + this.password);
-    }
 
     this.lastSyncedBooksMine = {};
     this.lastSyncedBooksShared = {};
@@ -32,9 +22,6 @@ export class Endpoint {
     return {
       url: this.url,
       headers: this.headers,
-      username: this.username,
-      password: this.password,
-      token: this.token,
       lastSyncedThreads: this.lastSyncedThreads,
       lastSyncedBooksMine: this.lastSyncedBooksMine,
       lastSyncedBooksShared: this.lastSyncedBooksMine,
