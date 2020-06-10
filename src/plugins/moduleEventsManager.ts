a06dbb30086e2e6be22a2f79cac76a23fb86b074
Mile Divovic <md2950@nyu.edu>
2020-06-10 13:53:29 -0700

    add auth and validation functions for moduleEventsManager

--- src/plugins/moduleEventsManager.ts
+++ src/plugins/moduleEventsManager.ts
@@ -1,7 +1,7 @@
 import { PeBLPlugin } from "../models/peblPlugin";
 import { SessionDataManager } from "../interfaces/sessionDataManager";
 import { ModuleEventsManager } from "../interfaces/moduleEventsManager";
-import { ModuleEvent } from "../models/moduleEvent";
+import { ModuleEvent, ModuleRating, ModuleFeedback, ModuleExample, ModuleExampleRating, ModuleExampleFeedback, ModuleRemovedEvent } from "../models/moduleEvent";
 import { generateUserModuleEventsKey, generateModuleEventsKey, LogCategory, Severity } from "../utils/constants";
 import { MessageTemplate } from "../models/messageTemplate";
 import { PermissionSet } from "../models/permission";
@@ -37,27 +37,57 @@ export class DefaultModuleEventsManager extends PeBLPlugin implements ModuleEven
   }
 
   validateGetModuleEvents(payload: { [key: string]: any }): boolean {
-    return false;
+    return true;
   }
 
   authorizeGetModuleEvents(username: string, permissions: PermissionSet, payload: { [key: string]: any }): boolean {
-    return false;
+    let canUser = (username == payload.identity) && (permissions.user[payload.requestType])
+    let canGroup = permissions.group[payload.identity] && permissions.group[payload.identity][payload.requestType]
+
+    return canUser || canGroup;
   }
 
   validateSaveModuleEvents(payload: { [key: string]: any }): boolean {
+    if (payload.smts && Array.isArray(payload.smts) && payload.smts.length > 0) {
+      for (let event in payload.smts) {
+        if (ModuleRating.is(payload.smts[event]))
+          payload.smts[event] = new ModuleRating(payload.smts[event]);
+        else if (ModuleFeedback.is(payload.stmts[event]))
+          payload.smts[event] = new ModuleFeedback(payload.stmts[event]);
+        else if (ModuleExample.is(payload.stmts[event]))
+          payload.stmts[event] = new ModuleExample(payload.stmts[event]);
+        else if (ModuleExampleRating.is(payload.stmts[event]))
+          payload.stmts[event] = new ModuleExampleRating(payload.stmts[event]);
+        else if (ModuleExampleFeedback.is(payload.stmts[event]))
+          payload.stmts[event] = new ModuleExampleFeedback(payload.stmts[event]);
+        else if (ModuleRemovedEvent.is(payload.stmts[event]))
+          payload.stmts[event] = new ModuleRemovedEvent(payload.stmts[event]);
+        else
+          return false;
+      }
+      return true;
+    }
     return false;
   }
 
   authorizeSaveModuleEvents(username: string, permissions: PermissionSet, payload: { [key: string]: any }): boolean {
-    return false;
+    let canUser = (username == payload.identity) && (permissions.user[payload.requestType])
+    let canGroup = permissions.group[payload.identity] && permissions.group[payload.identity][payload.requestType]
+
+    return canUser || canGroup;
   }
 
   validateDeleteModuleEvent(payload: { [key: string]: any }): boolean {
+    if (payload.xId && typeof payload.xId == "string")
+      return true;
     return false;
   }
 
   authorizeDeleteModuleEvent(username: string, permissions: PermissionSet, payload: { [key: string]: any }): boolean {
-    return false;
+    let canUser = (username == payload.identity) && (permissions.user[payload.requestType])
+    let canGroup = permissions.group[payload.identity] && permissions.group[payload.identity][payload.requestType]
+
+    return canUser || canGroup;
   }
 
   getModuleEvents(identity: string, callback: ((events: ModuleEvent[]) => void)): void {
@@ -70,14 +100,10 @@ export class DefaultModuleEventsManager extends PeBLPlugin implements ModuleEven
   }
 
   saveModuleEvents(identity: string, events: ModuleEvent[], callback: ((success: boolean) => void)): void {
-    let arr = [];
-    for (let event of events) {
-      let eventStr = JSON.stringify(event);
-      arr.push(generateModuleEventsKey(event.id));
-      arr.push(eventStr);
-      this.sessionData.queueForLrs(eventStr);
+    for (let stmt of events) {
+      let stmtStr = JSON.stringify(stmt);
+      this.sessionData.queueForLrs(stmtStr);
     }
-    this.sessionData.setHashValues(generateUserModuleEventsKey(identity), arr);
     callback(true);
   }
 
