7a104411267580ef81464b737c16e569da16f53f
Aaron Veden <aaron.veden@eduworks.com>
2020-06-15 11:38:54 -0700

    fixed lrs path

--- src/models/endpoint.ts
+++ src/models/endpoint.ts
@@ -1,5 +1,6 @@
 export class Endpoint {
-  readonly url: string;
+  readonly host: string;
+  readonly path: string;
   readonly headers: { [key: string]: any };
   readonly lastSyncedThreads: { [key: string]: Date }
   readonly lastSyncedBooksMine: { [key: string]: Date }
@@ -8,9 +9,9 @@ export class Endpoint {
   readonly lastSyncedModules: { [key: string]: Date }
 
   constructor(raw: { [key: string]: any }) {
-    this.url = raw.url;
+    this.host = raw.host;
+    this.path = raw.path ? (raw.path.endsWith("/") ? raw.path : raw.path + "/") : '/';
     this.headers = raw.headers;
-
     this.lastSyncedBooksMine = {};
     this.lastSyncedBooksShared = {};
     this.lastSyncedThreads = {};
@@ -20,7 +21,8 @@ export class Endpoint {
 
   toObject(urlPrefix: string = ""): { [key: string]: any } {
     return {
-      url: this.url,
+      host: this.host,
+      path: this.path,
       headers: this.headers,
       lastSyncedThreads: this.lastSyncedThreads,
       lastSyncedBooksMine: this.lastSyncedBooksMine,
