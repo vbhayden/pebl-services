4dc117b6400c3e9c7e65e4f8613747f09d064907
Mile Divovic <md2950@nyu.edu>
2020-06-23 11:11:31 -0700

    revert change to PREFIX_PEBL const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const NAMESPACE_USER_MESSAGES = "user-";
 export const PREFIX_PEBL_THREAD = "peblThread://";
-export const PREFIX_PEBL = "";
+export const PREFIX_PEBL = "pebl://";
 export const PREFIX_PEBL_EXTENSION = "https://www.peblproject.com/definitions.html#";
 
 // export const USER_PREFIX = "user-";
