05b18f745f83f67a46ad57e27dbb15aab15171f5
trey.hayden.ctr@adlnet.gov <trey.hayden.ctr@adlnet.gov>
2021-07-26 13:34:53 -0400

    adding dedicated machine config

---
+++ certbot/generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Clear out the old / placeholder certs
+rm -rf ./certbot/etc
+
+## Take down the actual PeBL Services stuff
+docker-compose down
+
+## Run Certbot to generate SSL certs for PeBL, this will also
+## spin up an Nginx container to handle the acme challenge
+docker-compose run certbot \
+	certonly --webroot \
+	--file docker-compose-certbot.yml \
+	--register-unsafely-without-email --agree-tos \
+	--webroot-path=/data/letsencrypt \
+	-d $1
+
+## Take the placeholder Nginx container down
+docker-compose down --file docker-compose-certbot.yml
+
+## Bring the PeBL Services stuff back up
+docker-compose up -d
---
+++ certbot/renew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+## Take down the actual PeBL Services stuff
+docker-compose down
+
+docker-compose run certbot \
+	renew --webroot \
+	--register-unsafely-without-email \
+	--file docker-compose-certbot.yml \
+	--agree-tos \
+	--no-random-sleep-on-renew \
+	--webroot-path=/data/letsencrypt
+
+## Take the placeholder Nginx container down
+docker-compose down --file docker-compose-certbot.yml
+
+## Bring the PeBL Services stuff back up
+docker-compose up -d
